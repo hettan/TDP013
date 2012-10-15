60f19d7c15fa7f321603105dd5480b8f4062cf87
Marcus <mennon@PowerStation.(none)>
2012-10-15 18:36:02 +0200

    more selenium

--- projekt/sele/test.py
+++ projekt/sele/test.py
@@ -9,13 +9,14 @@ driver = webdriver.Firefox()
 driver.get("http://www-und.ida.liu.se/~marhe414/")
 
 stat = True
-    
+#window_maximize
+
 def reg(user,pw,rname):
     reguser = driver.find_element_by_id("reguser")
     regpass = driver.find_element_by_id("regpass")
     regname = driver.find_element_by_id("regname")
 
-    time.sleep(0.5) #Need
+    time.sleep(1) #Need
     
     reguser.clear()
     regpass.clear()
@@ -25,13 +26,11 @@ def reg(user,pw,rname):
     regpass.send_keys(pw)
     regname.send_keys(rname)
     
-    register = driver.find_element_by_id("register")
-    register.click()
+    driver.find_element_by_id("register").click()
     
-    time.sleep(1.5) #Need to wait for server response
-
-    status = driver.find_element_by_id("err")
-    return status.get_attribute("innerHTML")
+    time.sleep(2) #Need to wait for server response
+    
+    return driver.find_element_by_id("err").get_attribute("innerHTML")
 
 def login(un,pw):
     user = driver.find_element_by_id("user")
@@ -43,9 +42,8 @@ def login(un,pw):
     user.send_keys(un)
     passw.send_keys(pw)
     
-    login = driver.find_element_by_id("go")
-    login.click()
-    
+    driver.find_element_by_id("go").click()
+  
     time.sleep(1.5)
 
     try:
@@ -61,6 +59,7 @@ while(stat):
     ###########################################
     # Testing The Register Functionality
     ###########################################
+    
     regbtn = driver.find_element_by_id("reg")
     regbtn.click()
     if reg("","","") != "Please Enter A Username":
@@ -79,22 +78,20 @@ while(stat):
         print "wrong response4"
         break
 
-    # if reg("1","1","mac") != "Congratulations! Your account has been successfully registred.":
-    #    print "wrong response5"
-    #    break
-    # regbtn = driver.find_element_by_id("reg")
-    # regbtn.click()
+    if reg("1","1","mac") != "Congratulations! Your account has been successfully registred.":
+        print "wrong response5"
+        break
+    
+    regbtn = driver.find_element_by_id("reg")
+    regbtn.click()
     
     if reg("1","1","mac") != "The username is already in use, please try another one!":
        print "wrong response6"
        break
     
-    closereg = driver.find_element_by_id("closereg")
-    closereg.click()
-    
-    # if reg("2","2","pc") != "Congratulations! Your account has been successfully registred.":
-    #     print "wrong response7"
-    #     break
+    if reg("2","2","pc") != "Congratulations! Your account has been successfully registred.":
+        print "wrong response7"
+        break
 
     print "Registration test complete"
 
@@ -116,38 +113,42 @@ while(stat):
 
     print "Login test complete"
 
+
+    ###########################################
+    # Testing Addfriend  Functionality
+    ###########################################
+
+    
     ###########################################
     # Testing Friendlist  Functionality
     ###########################################
 
-    friends = driver.find_element_by_id("friends")
-    friends.click()
-
+    driver.find_element_by_id("friends").click()
+    time.sleep(2)
     
-    
+    driver.find_elements_by_xpath("//*[@class='friendlisted']")[0].click()
+    driver.find_element_by_id("friends").click()
+    time.sleep(2)
+        
     ###########################################
-    # Testing *******  Functionality
-    ###########################################
-
-    
-    ###########################################
-    # Testing *******  Functionality
+    # Testing Wall Functionality
     ###########################################
 
-    
-    ###########################################
-    # Testing *******  Functionality
-    ###########################################
+    driver.find_element_by_id("post").send_keys("Selenium tells you that it can write on the wall")
+    driver.find_element_by_id("sendpost").click()
+    time.sleep(1)
 
     
     ###########################################
     # Testing *******  Functionality
     ###########################################
 
-
-
+    
+    ###########################################
+    # Testing *******  Functionality
+    ###########################################
 
 
     ##################################
     print "Test Done"
-    break #last break
+    stat = False
