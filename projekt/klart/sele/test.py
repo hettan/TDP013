eac5ae66f91f6b025c0c8e191ea74aa72f3061fb
Hellgren <hellgren@hellgren-VPCF12M1E.(none)>
2012-10-16 12:36:05 +0200

    proj done

--- projekt/klart/sele/test.py
+++ projekt/klart/sele/test.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException  
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
 import time
 
 driver = webdriver.Firefox()
@@ -81,7 +84,8 @@ while(stat):
     if reg("1","1","mac") != "Congratulations! Your account has been successfully registred.":
         print "wrong response5"
         break
-    
+
+    time.sleep(1)
     regbtn = driver.find_element_by_id("reg")
     regbtn.click()
     
@@ -93,12 +97,29 @@ while(stat):
         print "wrong response7"
         break
 
+    
+    regbtn = driver.find_element_by_id("reg")
+    regbtn.click()
+    time.sleep(1)
+    reg("3","3","derp")
+    time.sleep(1)
+    regbtn = driver.find_element_by_id("reg")
+    regbtn.click()
+    reg("4","4","zomg")
+    time.sleep(1)
+    regbtn = driver.find_element_by_id("reg")
+    regbtn.click()
+    time.sleep(1)
+    reg("5","5","lulz")
+
     print "Registration test complete"
 
     ###########################################
     # Testing The Login Functionality
     ###########################################
-
+    
+    time.sleep(2)
+    
     if login("","") != "Wrong Password or Username":
         print "wrong response8"
         break
@@ -113,42 +134,81 @@ while(stat):
 
     print "Login test complete"
 
-
-    ###########################################
-    # Testing Addfriend  Functionality
-    ###########################################
-
     
     ###########################################
-    # Testing Friendlist  Functionality
+    # Testing  links   Functionality
     ###########################################
 
     driver.find_element_by_id("friends").click()
     time.sleep(2)
+    if driver.find_element_by_id("topic").get_attribute("innerHTML") != " Friends ":
+        print "wrong response10"
+        break
     
-    driver.find_elements_by_xpath("//*[@class='friendlisted']")[0].click()
+    driver.find_element_by_id("profile").click()
+    time.sleep(1)
+    if driver.find_element_by_id("topic").get_attribute("innerHTML") != " Wall ":
+        print "wrong response11"
+        break
     driver.find_element_by_id("friends").click()
-    time.sleep(2)
-        
+    time.sleep(1)
+    print "Links test complete"
+
+    ###########################################
+    # Testing  Search  Functionality
+    ###########################################
+
+    driver.find_element_by_id("search").send_keys("p")
+    time.sleep(1)
+    driver.find_element_by_id("search").send_keys("\n")
+    time.sleep(1)
+    driver.find_element_by_id("2").click()
+    time.sleep(1)
+    if driver.find_element_by_id("username").get_attribute("innerHTML") != "2":
+        print "wrong response12"
+        break
+    
+    print "Search test complete"
+    
+    ###########################################
+    # Testing Friend Functionality
+    ###########################################
+
+    driver.find_element_by_id("friendadd").click()
+    time.sleep(1)
+    driver.find_element_by_id("friends").click()
+    time.sleep(1)
+
+    driver.find_element_by_id("2").click()
+   # driver.find_elements_by_xpath("//*[@class='friendlisted']")[1].click()
+    time.sleep(1)
+    
+    if driver.find_element_by_id("username").get_attribute("innerHTML") != "2":
+        print "wrong response13"
+        break
+    time.sleep(1)
+
+    print "Friends test complete"
+    
     ###########################################
     # Testing Wall Functionality
     ###########################################
 
-    driver.find_element_by_id("post").send_keys("Selenium tells you that it can write on the wall")
+    driver.find_element_by_id("post").send_keys("Testing sent by user1")
+    time.sleep(1)
     driver.find_element_by_id("sendpost").click()
     time.sleep(1)
+#    driver.find_element_by_id("friends").click()
+#    time.sleep(1)
+#    driver.find_element_by_id("2").click()
+#    time.sleep(1)
 
+    time.sleep(6)
     
-    ###########################################
-    # Testing *******  Functionality
-    ###########################################
-
+    if driver.find_elements_by_xpath("//*[@class='posts']//pre")[0].get_attribute("innerHTML") != "Testing sent by user1":
+        print "wrong response14"
+        break
     
-    ###########################################
-    # Testing *******  Functionality
-    ###########################################
-
-
     ##################################
-    print "Test Done"
+    print "Done ~~~~~~~~ Done "
     stat = False
